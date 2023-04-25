fa6c3e9c0a9db66692f482e2bf7fb48216ef6e4d
JoelACedeno <123585946+JoelACedeno@users.noreply.github.com>
2023-04-25 15:08:05 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, request, redirect, flash 
+from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey as survey 
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret_key"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+debug = DebugToolbarExtension(app)
+
+responses = []
+
+
+@app.route("/")
+def show_survey_start():
+    """show survey """
+    return render_template("survey_start.html", survey=survey)
+
+
+@app.route("/questions/<int:qid>")
+def show_question(qid):
+    """display current question"""
+
+    if (responses is None):
+        #trying to access question page too soon
+        redirect("/")
+    
+    if (len(responses) == len(survey.questions)):
+        #all questions answered send to thank you screen 
+        return redirect("/complete")
+    
+    if (len(responses) != qid):
+        #trying to access question out of order
+        flash(f"Invalid question id: {qid}.")
+        redirect(f"/questions/{len(responses)}")
+
+    question = survey.questions[qid]
+    return render_template("question.html", question_num=qid, question=question)
+
+
+@app.route("/answer", methods=["POST"])
+def handle_question():
+    """save response and redirect next question"""
+
+    #get the response choice
+    choice = request.form["answer"]
+    #add choice to responses list
+    responses.append(choice)
+
+    if (len(responses) == len(survey.questions)):
+        #all questions answered send to thank you screen
+        return redirect("/complete")
+    else:
+        return redirect(f"/questions/{len(responses)}")
+
+
+@app.route("/complete")
+def complete():
+    """show thank you screen"""
+    return render_template("completion.html")
